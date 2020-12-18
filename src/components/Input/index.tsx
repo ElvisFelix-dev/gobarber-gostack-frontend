e25c09722059431c81b8bab154f370b998f861cf
Elvis Felix <elvisjaspion@gmail.com>
2020-12-18 00:24:24 -0300

    Trabalhando com formularios

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,9 @@
-import React, {InputHTMLAttributes} from 'react';
+import React, {InputHTMLAttributes, useEffect, useRef} from 'react';
 import { IconBaseProps } from 'react-icons'
+import { useField } from '@unform/core';
 
 import { Container } from './styles';
+import { promises } from 'fs';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
@@ -9,10 +11,22 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input: React.FC<InputProps> = ({ icon: Icon, ...rest}) => {
+  const inputRef = useRef(null);
+  // eslint-disable-next-line no-restricted-globals
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    })
+  }, [fieldName, registerField]);
+
   return (
     <Container>
       { Icon && <Icon size={20} />}
-      <input {...rest}/>
+      <input ref={inputRef} {...rest}/>
     </Container>
   )
 }
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { FiArrowLeft, FiMail, FiUser ,FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -9,37 +10,43 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignUp: React.FC = () => (
-  <Container>
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  };
+
+  return (
+    <Container>
     <Background />
 
-    <Content>
-      <img src={logoImg} alt="GoBaber"/>
+      <Content>
+        <img src={logoImg} alt="GoBaber"/>
 
-      <form>
-        <h1>Faça seu cadastro</h1>
+        <Form onSubmit={handleSubmit} >
+          <h1>Faça seu cadastro</h1>
 
-        <Input name="name" icon={FiUser} placeholder="Nome" />
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-        <Button type="submit">Cadastrar</Button>
-      </form>
+         <Button type="submit">Cadastrar</Button>
+        </Form>
 
-      <a href="login">
-        <FiArrowLeft />
-        Voltar para logon
-      </a>
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
 
-    </Content>
+      </Content>
 
-  </Container>
-)
+    </Container>
+  )
+}
 
 export default SignUp;
